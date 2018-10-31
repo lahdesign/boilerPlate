3f1f6acaaf679c98f365811de585440745f32e9d
lahdesign <lahdesign@gmail.com>
2018-10-31 10:25:43 -0400

    initial setup

---
+++ main.js
@@ -0,0 +1,16 @@
+const onNameClick = function () {
+  //  console.log('got into function')
+  //   $('#message').text('Welcome to the moon.')
+  //   $('#message').css('background-Color', 'blue')
+  //   setTimeout(() => $('#message').text(''), 3000)
+  //   $('.container').addClass('animated rollOut');
+  //   $('#welcomePage').addClass('animated rollIn')
+  }
+
+const addHandlers = () => {
+    // $('#nextView').on('click', onMoonClick)
+  }
+
+  $(() => {
+    addHandlers()
+  })
