59ade9834229775b34005a4e0257dd272fd71991
Lara Henderson <39603232+lahdesign@users.noreply.github.com>
2023-02-05 20:03:17 -0500

    connected javascript document, added animation

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const onNameClick = function () {
-  //  console.log('got into function')
-  //   $('#message').text('Welcome to the moon.')
-  //   $('#message').css('background-Color', 'blue')
-  //   setTimeout(() => $('#message').text(''), 3000)
-  //   $('.container').addClass('animated rollOut');
-  //   $('#welcomePage').addClass('animated rollIn')
+const onButtonClick = function () {
+   console.log('got into function')
+    // $('#message').text('Welcome to the moon.')
+    // $('#message').css('background-Color', 'blue')
+    // setTimeout(() => $('#message').text(''), 3000)
+    $('.front-card').addClass('animated hinge');
+    // $('#welcomePage').addClass('animated rollIn')
   }
 
 const addHandlers = () => {
-    // $('#nextView').on('click', onMoonClick)
+    $('#button').on('click', onButtonClick)
   }
 
   $(() => {
